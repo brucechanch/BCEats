06cc942d6efdbbf10ad0ad9151f55a4eabbe708f
brucechanch <brucechanch@gmail.com>
2024-04-25 10:34:14 +0800

    update_user_profile_request_done

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,7 +1,10 @@
 import UserProfileForm from '@/form/user-profile-form/UserProfileForm'
 
+import { useUpdateMyUser } from '@/api/MyUserApi'
+
 const UserProfilePage = () => {
-  return <UserProfileForm />
+  const { updateUser, isLoading } = useUpdateMyUser()
+  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />
 }
 
 export default UserProfilePage
